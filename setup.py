66d0f5582f98e4141815db8a64eca1c6584a0d97
Keiron O'Shea <keiron.oshea@gmail.com>
2023-07-04 16:33:30 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(this_directory, "README.md")) as f:
 
 setup(
     name="codingerrors",
-    version="0.0.9",
+    version="0.1.0",
     description="Coding Dashboard Error Checker",
     long_description=long_description,
     long_description_content_type="text/markdown",
